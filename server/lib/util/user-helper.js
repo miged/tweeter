8c648950656e11f4209a75c0367d961564c50110
Miguel <mige.d@outlook.com>
2021-11-14 21:49:10 -0500

    fix lint issues

--- server/lib/util/user-helper.js
+++ server/lib/util/user-helper.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const Chance      = require("chance"),
-      chance      = new Chance();
+  chance      = new Chance();
 
 const md5 = require('md5');
 
@@ -9,15 +9,15 @@ const md5 = require('md5');
 module.exports = {
   generateRandomUser: () => {
     const gender    = chance.gender();
-    const firstName = chance.first({gender: gender});
+    const firstName = chance.first({gender});
     const lastName  = chance.last();
-    const userName  = firstName + " " + lastName;
+    const userName  = `${firstName  } ${  lastName}`;
     
     let userHandle = "@";
     if (Math.random() > 0.5) {
-      let prefix    = chance.prefix({gender: gender});
+      let prefix    = chance.prefix({gender});
       prefix = prefix.replace(".", "");
-      userHandle += prefix
+      userHandle += prefix;
     }
 
     userHandle += lastName;
@@ -32,10 +32,10 @@ module.exports = {
       Female: ["https://i.imgur.com/nlhLi3I.png","https://i.imgur.com/z5LNkkB.png","https://i.imgur.com/v0JXau2.png","https://i.imgur.com/lRUnDgU.png", "https://i.imgur.com/3GvwNBf.png"],
       Male: ["https://i.imgur.com/73hZDYK.png","https://i.imgur.com/5fUVPRP.png","https://i.imgur.com/DVpDmdR.png","https://i.imgur.com/2WZtOD6.png", "https://i.imgur.com/ilT4JDe.png"]
     
-    }
+    };
     
-    const avatarArray = avatars[gender]
-    const userAvatar = avatarArray[Math.floor(Math.random()*avatarArray.length)]
+    const avatarArray = avatars[gender];
+    const userAvatar = avatarArray[Math.floor(Math.random() * avatarArray.length)];
   
 
     return {
