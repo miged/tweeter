41d02b277533780a40432867ba46586beccd99b3
Miguel <mige.d@outlook.com>
2021-12-05 14:47:33 -0500

    reset counter on submit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
   // Character counter
   $("#tweet-text").on('input', function() {
-    const counter = $("#tweet-text").parent().find(".counter");
+    const $counter = $(".counter");
     const charCount = 140 - this.value.length;
-    counter.val(charCount);
+    $counter.val(charCount);
 
     if (charCount < 0) {
-      counter.css("color", "red");
+      $counter.css('color', 'red');
     } else {
-      counter.css("color", "#545149");
+      $counter.css("color", "#545149");
     }
+  });
+
+  $('#tweet-form').submit(function() {
+    $('.counter').val(140);
   });
 });
